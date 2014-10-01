8456f8250f3617d772ff8eaab2f311e70d52a077
Ivaylo Spasov <ivaylo.pif@gmail.com>
2014-10-01 16:13:56 +0300

    modified server.js

--- server.js
+++ server.js
@@ -12,11 +12,27 @@ var inputImg = {
 var inputImgPath = path.join(inputImg.imgPath, inputImg.rawImgPath, inputImg.imgName)
 
 var thumbs = [
-  { w: 800, h: 800, path: path.join(inputImg.imgPath, '/fullsize', inputImg.imgName) },
-  { w: 400, h: 400, path: path.join(inputImg.imgPath, '/normal', inputImg.imgName) },
-  { w: 128, h: 128, path: path.join(inputImg.imgPath, '/small', inputImg.imgName) },
+  { 
+  	w: 800, 
+  	h: 800, 
+  	path: path.join(inputImg.imgPath, '/fullsize', inputImg.imgName) 
+  },
+  { 
+  	w: 400, 
+  	h: 400, 
+  	path: path.join(inputImg.imgPath, '/normal', inputImg.imgName) 
+  },
+  { 
+  	w: 128, 
+  	h: 128, 
+  	path: path.join(inputImg.imgPath, '/small', inputImg.imgName) 
+  },
   //Следващите размери ще са за квадратни thumbnail-и
-  { w: 75,  h: 75,  path: path.join(inputImg.imgPath, '/xsmall', inputImg.imgName) }
+  { 
+  	w: 75,  
+  	h: 75,  
+  	path: path.join(inputImg.imgPath, '/xsmall', inputImg.imgName) 
+  }
 ]
 
 gm(inputImgPath).format(function(err, value){
