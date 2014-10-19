56388fa2d01d04759bd5c2ac91a63833262bf2c0
ivaylo Spasov <ivaylo.pif@gmail.com>
2014-10-19 15:54:49 +0300

    Added function processImage

--- server.js
+++ server.js
@@ -11,64 +11,65 @@ var inputImg = {
 
 var inputImgPath = path.join(inputImg.imgPath, inputImg.rawImgPath, inputImg.imgName)
 
-var thumbs = [
-  { 
-  	w: 800, 
-  	h: 800, 
-  	path: '/fullsize' 
+var strategies = {
+  fullsize: {
+  	w: 800,
+  	h: 800,
+  	path: '/fullsize'
   },
-  { 
-  	w: 400, 
-  	h: 400, 
-  	path: '/normal' 
+  normal : {
+  	w: 400,
+  	h: 400,
+  	path: '/normal'
   },
-  { 
-  	w: 128, 
-  	h: 128, 
+  small : {
+  	w: 128,
+  	h: 128,
   	path: '/small'
   },
   //Следващите размери ще са за квадратни thumbnail-и
-  { 
-  	w: 75,  
-  	h: 75,  
-  	path: '/xsmall' 
+  xsmall : {
+  	w: 75,
+  	h: 75,
+  	path: '/xsmall'
   }
-]
-
-/*
-function(images) {
-  console.log("processing : " + images.length + " image(s)");
-  images.forEach(function (image, imageIndex) {
-  
-  })
 }
-*/
-gm(inputImgPath).format(function(err, value){
-  if (value == 'JPEG') {
-    gm(inputImgPath)
-    .resize(inputImg.bufferImgWidth)
-    .stream(function (err, stdout, stderr) {
-    // before Resize the width is 1024px 
-      for (var i = 0; i < 3; i++) {
-      gm(stdout) // gm can read buffers ;)
-      .resize(thumbs[i].w)
-      .write(path.join(inputImg.imgPath, thumbs[i].path, inputImg.imgName), function (err, size) {
-        if (err) console.log('Error - ', error);
-        console.log('Created an image!');
-      });
-    };
-      for (var i = 3; i < thumbs.length; i++) {
-      gm(stdout) // gm can read buffers
-      .resize(thumbs[2].w, thumbs[2].h + ">")
-      .gravity('Center')
-      .extent(thumbs[3].w, thumbs[3].h)
-      .write(path.join(inputImg.imgPath, thumbs[i].path, inputImg.imgName), function (err, size) {
-        if (err) console.log('Error - ', error);
-        console.log('Created an image!');
-      });
-      };
+
+
+function processImage(inputImgPath, imgName) {
+    gm(inputImgPath).format(function(err, value){
+      if (value == 'JPEG') {
+        gm(inputImgPath)
+        .resize(inputImg.bufferImgWidth)
+        .stream(function (err, stdout, stderr) {
+        // before Resize the width is 1024px
+          for (key in strategies) {
+            switch (key) {
+                default:
+                  gm(stdout) // gm can read buffers ;)
+                  .resize(strategies[key].w)
+                  .write(path.join(inputImg.imgPath, strategies[key].path, imgName), function (err, size) {
+                    if (err) console.log('Error - ', error);
+                    console.log('Created an image!');
+                  });
+                  break;
+                case "xsmall":
+                  gm(stdout) // gm can read buffers
+                  .resize(strategies["small"].w, strategies["small"].h + ">")
+                  .gravity('Center')
+                  .extent(strategies[key].w, strategies[key].h)
+                  .write(path.join(inputImg.imgPath, strategies[key].path, imgName), function (err, size) {
+                    if (err) console.log('Error - ', error);
+                    console.log('Created an image!');
+                  });
+                  break;
+            }
+          };
+        });
+      } else {
+        console.log('Image is not JPEG');
+      }
     });
-  } else {
-    console.log('Image is not JPEG');
-  }
-});
+}
+
+processImage(inputImgPath, inputImg.imgName);
